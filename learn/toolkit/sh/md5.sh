ab0e2ee302e06c5f536786efe9791ff5efec1b06
sirdalang <1461021702@qq.com>
2020-12-07 23:28:30 +0800

    update

--- learn/toolkit/sh/md5.sh
+++ learn/toolkit/sh/md5.sh
@@ -1,3 +1,7 @@
-find ./ -type f -print0 | xargs -0 md5sum > ./md5.txt
+# find ./ -type f -print0 | xargs -0 md5sum > ./md5.txt
+
+find ./ -type f -print0 | xargs -0 md5sum sort -t ' ' -k 2 | > ./md5.txt
+
+# cat md5.txt | sort -t ' ' -k 2 > md5s.txt
 
 # md5sum -c my.md5
